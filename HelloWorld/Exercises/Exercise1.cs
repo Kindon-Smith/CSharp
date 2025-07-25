b7389c8521229c38949efb5b830f07c71e9d9a35
KindonSmith <kindon@bluelim3.com>
2025-07-24 18:22:46 -0700

    Pushing all changes before transitioning to new project

--- HelloWorld/Exercises/Exercise1.cs
+++ HelloWorld/Exercises/Exercise1.cs
@@ -1,7 +1,12 @@
 namespace HelloWorld.Exercises
 {
+    /// <summary>
+    /// First exercise demonstrating CLI arg and input prompting to
+    /// run different exercises.
+    /// </summary>
     public static class Exercise1
     {
+
         public static void Run()
         {
             Console.WriteLine("This is Exercise 1!");
