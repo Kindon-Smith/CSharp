5cce87d023471e492f9f497b1cd6be8d2e8b9191
KindonSmith <kindon@bluelim3.com>
2025-07-29 12:03:01 -0700

    AnagramCheck(string1, string2) takes two strings and returns true if they are anagrams
    Implementation uses single dictionary with incrementing/decrementing
    
    Validation of dictionary values implements LINQ

--- HelloWorld/Exercises/Exercise4.cs
+++ HelloWorld/Exercises/Exercise4.cs
@@ -111,6 +111,52 @@ public class Exercise4
         }
         return '\0';
     }
+
+    public static bool AnagramCheck(string input1, string input2)
+    {
+        if(input1 is null || input2 is null)
+        {
+            var nullInput = input1 == null ? nameof(input1) : nameof(input2);
+            throw new ArgumentNullException(nullInput, "Input string cannot be null.");
+        }
+        if (input1.Length != input2.Length)
+        {
+            return false;
+        }
+
+        // A few ways:
+        // dictionary containing char : int (for each char, count total)
+        // likely more complex memory usage
+        // 
+        // sorting - sorted strings will be identical if anagrams. likely a more
+        // complex runtime to sort two strings
+
+        Dictionary<char, int> letterCounts = new Dictionary<char, int>();
+
+        for (int i = 0; i < input1.Length; i++)
+        {
+            var letter1 = input1[i];
+            var letter2 = input2[i];
+            if (letterCounts.ContainsKey(letter1))
+            {
+                letterCounts[letter1] += 1;
+            }
+            else
+            {
+                letterCounts[letter1] = 1;
+            }
+
+            if (letterCounts.ContainsKey(letter2))
+            {
+                letterCounts[letter2] -= 1;
+            }
+            else
+            {
+                letterCounts[letter2] = -1;
+            }
+        }
+        return letterCounts.Values.All(count => count == 0);
+    }
     public static void Run()
     {
         /*string catDog = "catDog";
@@ -120,10 +166,17 @@ public class Exercise4
         Console.WriteLine(ReverseString("1"));
 
         Console.WriteLine(CountVowels("Hello"));
-        Console.WriteLine(CountVowels("Potatosandwichupinmygums"));*/
+        Console.WriteLine(CountVowels("Potatosandwichupinmygums"));
 
         Console.WriteLine(FirstNonRepeatingChar("Hello"));
-        Console.WriteLine(FirstNonRepeatingChar("Hhohh"));
+        Console.WriteLine(FirstNonRepeatingChar("Hhohh"));*/
 
+        Console.WriteLine(AnagramCheck("aba", "baa").ToString());
+        Console.WriteLine(AnagramCheck("aab", "baa").ToString());
+        Console.WriteLine(AnagramCheck("aba", "").ToString());
+        Console.WriteLine(AnagramCheck("ab", "baa").ToString());
+        Console.WriteLine(AnagramCheck("asdfasdvzxcv", "baafba").ToString());
+        Console.WriteLine(AnagramCheck("1", "2").ToString());
+        //Console.WriteLine(AnagramCheck("aba", null).ToString());
     }
 }
