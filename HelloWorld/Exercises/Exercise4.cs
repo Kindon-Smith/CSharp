6e80ecbfd40b721c1ce0cc449f16fd73d4052b53
KindonSmith <kindon@bluelim3.com>
2025-07-29 16:38:47 -0700

    Updated formatting and printing to more easily demo algo functions

--- HelloWorld/Exercises/Exercise4.cs
+++ HelloWorld/Exercises/Exercise4.cs
@@ -11,23 +11,23 @@ public class Exercise4
 
     public static void Run()
     {
+        Console.WriteLine("Running Exercise 4...");
+        Console.WriteLine($"{nameof(ReverseString)}: {ReverseString.Execute("catDog")}");
+        Console.WriteLine($"{nameof(ReverseString)} '': {ReverseString.Execute("")}");
 
-        Console.WriteLine($"Reversing 'catDog': {ReverseString.Execute("catDog")}");
-        Console.WriteLine($"Reversing '': {ReverseString.Execute("")}");
+        Console.WriteLine($"{nameof(FindMax)}: [ 1, 2, 3, 4, 5 ] {FindMax.Execute(new int[] { 1, 2, 3, 4, 5 })}");
+        Console.WriteLine($"{nameof(FindMax)}: [ 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 ] {FindMax.Execute(new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 })}");
 
-        Console.WriteLine($"Finding max of: [ 1, 2, 3, 4, 5 ] {FindMax.Execute(new int[] { 1, 2, 3, 4, 5 })}");
-        Console.WriteLine($"Finding max of: [ 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 ] {FindMax.Execute(new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 })}");
+        Console.WriteLine($"{nameof(CountVowels)}: 'looh': {CountVowels.Execute("Hello")}");
+        Console.WriteLine($"{nameof(CountVowels)}: 'Potatosandwichupinmygums': {CountVowels.Execute("Potatosandwichupinmygums")}");
 
-        Console.WriteLine($"Counting vowels in 'looh': {CountVowels.Execute("Hello")}");
-        Console.WriteLine($"Counting vowels in 'Potatosandwichupinmygums': {CountVowels.Execute("Potatosandwichupinmygums")}");
+        Console.WriteLine($"{nameof(FirstNonRepeatingChar)}: Finding first non-repeating character in 'loohl': {FirstNonRepeatingChar.Execute("loohl")}");
+        Console.WriteLine($"{nameof(FirstNonRepeatingChar)}: Finding first non-repeating character in 'Hhohh': {FirstNonRepeatingChar.Execute("Hhohh")}");
 
-        Console.WriteLine($"Finding first non-repeating character in 'loohl': {FirstNonRepeatingChar.Execute("loohl")}");
-        Console.WriteLine($"Finding first non-repeating character in 'Hhohh': {FirstNonRepeatingChar.Execute("Hhohh")}");
+        Console.WriteLine($"{nameof(AnagramCheck)}: Determining if 'aba' and 'baa' are anagrams: {AnagramCheck.Execute("aba", "baa")}");
+        Console.WriteLine($"{nameof(AnagramCheck)}: Determining if '' and 'baa' are anagrams: {AnagramCheck.Execute("", "baa")}");
 
-        Console.WriteLine($"Determining if 'aba' and 'baa' are anagrams: {AnagramCheck.Execute("aba", "baa")}");
-        Console.WriteLine($"Determining if '' and 'baa' are anagrams: {AnagramCheck.Execute("", "baa")}");
-
-        Console.WriteLine(LengthOfLongestSubstring.Execute($"Longest Substring of string: abcabcbb"));
-        Console.WriteLine(LengthOfLongestSubstring.Execute($"Longest Substring of string: bbbbb"));
+        Console.WriteLine($"{nameof(LengthOfLongestSubstring)}: Longest Substring of string: abcabcbb: {LengthOfLongestSubstring.Execute("abcabcbb")}");
+        Console.WriteLine($"{nameof(LengthOfLongestSubstring)}: Longest Substring of string: bbabcb: {LengthOfLongestSubstring.Execute("bbcb")}");
     }
 }
